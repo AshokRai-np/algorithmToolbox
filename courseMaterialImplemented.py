56d7d7aa81a8da2442b9723e2d49070debc1947a
RaiAshok0915 <10ashok180rai90@gmail.com>
2022-02-02 18:33:02 +0545

    inCourseContent new knapsack class with with no item repetition solved

--- courseMaterialImplemented.py
+++ courseMaterialImplemented.py
@@ -1,3 +1,5 @@
+# Knapsack problem with DP repeating items with fractioning not allowed
+
 def knapsack(itemValue, itemWeight, capacity):
     value = list()
     for w in range(capacity):
@@ -9,7 +11,28 @@ def knapsack(itemValue, itemWeight, capacity):
                    value[w] = val
     return value[w]
 
+# capacity = 7
+# itemWeight = [3, 2, 5, 4]
+# itemValue = [30, 20, 10, 55]
+# print(knapsack(itemValue, itemWeight, capacity))
+# ---------------------------------------------------------------------------------
+
+#Knapsack problem using DP, repetition of items not allowed with fractioning not allowed
+def knapsackNoRepetition(itemValue, itemWeight, capacity):
+    rows, cols = (len(itemWeight), capacity)
+    value = [[0 for i in range(cols)] for j in range(rows)]
+    for i in range(len(itemValue)):
+        for w in range(capacity):
+            value[i][w] = value[i -1][w]
+            if itemWeight[i] <= w:
+                val = value[i-1][w - itemWeight[i]] + itemValue[i]
+                if val > value[i][w]:
+                    value[i][w] = val
+    return value[i][w]
+# ---------------------------------------------------------------------------------------
+
+
 capacity = 7
 itemWeight = [3, 2, 5, 4]
 itemValue = [30, 20, 10, 55]
-print(knapsack(itemValue, itemWeight, capacity))
+print(knapsackNoRepetition(itemValue, itemWeight, capacity))
